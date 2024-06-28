a54368a70b9efd1b566c2ded7763c38b5289ec8a
nayeong lee <lny021102@gmail.com>
2024-06-28 13:50:44 +0900

    [ADD] 모바일 접근 차단

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './Components/Layout';
 import App from './pages/MainPage/App';
 import MyPage from './pages/SubPage/MyPage/MyPage';
 import ScrollToTop from './Components/ScrollToTop';
 import { AuthProvider } from './pages/SubPage/AuthContext';
+import { isMobile, isTablet } from 'react-device-detect';
 
 // 라우터 파일
 
 const AppRouter = () => {
+    useEffect(() => {
+        if (isMobile || isTablet) {
+            alert('데스크톱으로만 접속이 가능합니다.');
+        }
+    }, []);
+
+    if (isMobile || isTablet) {
+        return null; // 모바일 및 태블릿일 경우 아무것도 렌더링하지 않음
+    }
+
     return (
         <AuthProvider>
             <Router>
