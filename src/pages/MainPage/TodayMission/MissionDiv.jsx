c1aaddc73ae1260e2308e276a66997104173828a
daeunleeeee <lde7953@gmail.com>
2024-06-29 02:46:11 +0900

    🚨 [ERROR] 미션 로딩 에러

--- src/pages/MainPage/TodayMission/MissionDiv.jsx
+++ src/pages/MainPage/TodayMission/MissionDiv.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled, { keyframes } from 'styled-components';
 import CustomFont from '../../../Components/Container/CustomFont';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useAuth } from '../../SubPage/AuthContext';
 
 const Container = styled.div`
   display: flex;
@@ -32,7 +34,7 @@ const moveUpDown = keyframes`
   }
 `;
 
-const MissionContainer = styled.button`
+const MissionContainer = styled.div`
   position: relative;
   width: 50%;
   min-height: 2rem;
@@ -57,27 +59,66 @@ const IconImage = styled.img`
 `;
 
 const App = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const { auth } = useAuth();
+  const [mission, setMission] = useState(null);
 
-    const camera = () => {
-        navigate('/camera');
+  useEffect(() => {
+    const fetchMission = async () => {
+      try {
+        const accessToken = localStorage.getItem('accessToken');
+        const response = await axios.get(`${import.meta.env.VITE_REACT_APP_SERVER}/missions/today`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        if (response.status === 200) {
+          setMission(response.data);
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 400) {
+          alert('미션 로딩 실패');
+        } else {
+          console.error('미션 로딩 실패', error);
+          alert('미션을 가져오지 못했습니다.');
+        }
+      }
     };
 
-    return (
-        <Container>
-            <Title>원하는 구단에 투표하세요!</Title>
-            <Description>
-                오늘의 미션을 수행하면 포인트를 받을 수 있습니다.<br />
-                포인트로 투표를 하거나 미션을 변경할 수 있습니다.<br />
-                미션을 수행하는 만큼 환경 보호에 동참할 수 있습니다.
-            </Description>
-            <MissionContainer onClick={camera}>
-                <CustomFont color='black' fontWeight='bold' font='2rem'>오늘의 미션</CustomFont>
-                <CustomFont color='black' font='1.5rem'>페트병 3개를 주워 버리세요!</CustomFont>
-                <IconImage src='icon_click.png' />
-            </MissionContainer>
-        </Container>
-    );
+    if (auth.isLoggedIn) {
+      fetchMission();
+    }
+  }, [auth.isLoggedIn]);
+
+  const camera = () => {
+    navigate('/camera');
+  };
+
+  return (
+    <Container>
+      <Title>원하는 구단에 투표하세요!</Title>
+      <Description>
+        오늘의 미션을 수행하면 포인트를 받을 수 있습니다.<br />
+        포인트로 응원하는 구단에 투표하고 기부해보세요. <br />
+        미션을 수행하며 선행을 실천해보세요!
+      </Description>
+      {auth.isLoggedIn ? (
+        <MissionContainer onClick={camera}>
+          <CustomFont color='black' fontWeight='bold' font='2rem'>오늘의 미션</CustomFont>
+          <CustomFont color='black' font='1.5rem'>{mission}[</CustomFont>
+          <IconImage src='icon_click.png' />
+        </MissionContainer>
+      ) : (
+
+        <MissionContainer>
+        <CustomFont color='black' fontWeight='bold' font='2rem'>오늘의 미션</CustomFont>
+        <CustomFont color='black' font='1.5rem'>          미션 수행을 위해 로그인을 해주세요.
+        </CustomFont>
+        <IconImage src='icon_click.png' />
+      </MissionContainer>
+      )}
+    </Container>
+  );
 };
 
 export default App;
