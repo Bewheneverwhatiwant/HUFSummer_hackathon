54c8d12ad73e15d66349cff20292ee37b331665c
nayeong lee <lny021102@gmail.com>
2024-06-29 03:13:03 +0900

    에러 해결 중

--- src/pages/MainPage/TodayMission/MissionDiv.jsx
+++ src/pages/MainPage/TodayMission/MissionDiv.jsx
@@ -59,66 +59,66 @@ const IconImage = styled.img`
 `;
 
 const App = () => {
-  const navigate = useNavigate();
-  const { auth } = useAuth();
-  const [mission, setMission] = useState(null);
+    const navigate = useNavigate();
+    const { auth } = useAuth();
+    const [missionContent, setMissionContent] = useState('');
+    const accessToken = localStorage.getItem('accessToken');
 
-  useEffect(() => {
-    const fetchMission = async () => {
-      try {
-        const accessToken = localStorage.getItem('accessToken');
-        const response = await axios.get(`${import.meta.env.VITE_REACT_APP_SERVER}/missions/today`, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-        if (response.status === 200) {
-          setMission(response.data);
+    useEffect(() => {
+        const fetchMission = async () => {
+            try {
+                const accessToken = localStorage.getItem('accessToken');
+                const response = await axios.get(`${import.meta.env.VITE_REACT_APP_SERVER}/missions/today`, {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                });
+                if (response.status === 200) {
+                    setMissionContent(response.data.content);
+                }
+            } catch (error) {
+                if (error.response && error.response.status === 400) {
+                    console.log(accessToken);
+                    //alert('미션 로딩 실패');
+                } else {
+                    console.error('미션 로딩 실패', error);
+                    alert('미션을 가져오지 못했습니다.');
+                }
+            }
+        };
+
+        if (auth.isLoggedIn) {
+            fetchMission();
         }
-      } catch (error) {
-        if (error.response && error.response.status === 400) {
-          alert('미션 로딩 실패');
-        } else {
-          console.error('미션 로딩 실패', error);
-          alert('미션을 가져오지 못했습니다.');
-        }
-      }
+    }, [auth.isLoggedIn]);
+
+    const camera = () => {
+        navigate('/camera');
     };
 
-    if (auth.isLoggedIn) {
-      fetchMission();
-    }
-  }, [auth.isLoggedIn]);
-
-  const camera = () => {
-    navigate('/camera');
-  };
-
-  return (
-    <Container>
-      <Title>원하는 구단에 투표하세요!</Title>
-      <Description>
-        오늘의 미션을 수행하면 포인트를 받을 수 있습니다.<br />
-        포인트로 응원하는 구단에 투표하고 기부해보세요. <br />
-        미션을 수행하며 선행을 실천해보세요!
-      </Description>
-      {auth.isLoggedIn ? (
-        <MissionContainer onClick={camera}>
-          <CustomFont color='black' fontWeight='bold' font='2rem'>오늘의 미션</CustomFont>
-          <CustomFont color='black' font='1.5rem'>{mission}[</CustomFont>
-          <IconImage src='icon_click.png' />
-        </MissionContainer>
-      ) : (
-
-        <MissionContainer>
-        <CustomFont color='black' fontWeight='bold' font='2rem'>오늘의 미션</CustomFont>
-        <CustomFont color='black' font='1.5rem'>          미션 수행을 위해 로그인을 해주세요.
-        </CustomFont>
-        <IconImage src='icon_click.png' />
-      </MissionContainer>
-      )}
-    </Container>
-  );
+    return (
+        <Container>
+            <Title>원하는 구단에 투표하세요!</Title>
+            <Description>
+                오늘의 미션을 수행하면 포인트를 받을 수 있습니다.<br />
+                포인트로 응원하는 구단에 투표하고 기부해보세요. <br />
+                미션을 수행하며 선행을 실천해보세요!
+            </Description>
+            {auth.isLoggedIn ? (
+                <MissionContainer onClick={camera}>
+                    <CustomFont color='black' fontWeight='bold' font='2rem'>오늘의 미션</CustomFont>
+                    <CustomFont color='black' font='1.5rem'>{missionContent}</CustomFont>
+                    <IconImage src='icon_click.png' />
+                </MissionContainer>
+            ) : (
+                <MissionContainer>
+                    <CustomFont color='black' fontWeight='bold' font='2rem'>오늘의 미션</CustomFont>
+                    <CustomFont color='black' font='1.5rem'>미션 수행을 위해 로그인을 해주세요.</CustomFont>
+                    <IconImage src='icon_click.png' />
+                </MissionContainer>
+            )}
+        </Container>
+    );
 };
 
 export default App;
