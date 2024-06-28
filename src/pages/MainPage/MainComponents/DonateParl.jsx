c18d1cefc1746cda2dc491eae795e6649af704b8
nayeong lee <lny021102@gmail.com>
2024-06-28 23:46:52 +0900

    [ADD] 메인페이지 디자인 수정

--- src/pages/MainPage/MainComponents/DonateParl.jsx
+++ src/pages/MainPage/MainComponents/DonateParl.jsx
@@ -30,7 +30,7 @@ const ToggleButton = styled.button`
 `;
 
 const DonateBox = styled.div`
-    width: 30%;
+    width: 55%;
     background-color: #EFEFEF;
     padding: 30px;
     
@@ -39,16 +39,16 @@ const DonateBox = styled.div`
 
 export default function DonateParl() {
     const DanateList = [
-        {'number' : '1', 'Teamlogo' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points' : '100'},
-        {'number' : '2', 'Teamlogo' : 'https://i.namu.wiki/i/nhKvnqKXbs7dbPMggKos010RqfYHxUoKgd4GOPjaN8HICce5HXZo6_zAQJPO8SNd-tMEthvTehq7ef-jzuRoww.svg', 'points' : '50'},
-        {'number' : '3', 'Teamlogo' : 'https://heroesbaseball.co.kr/html/front/web_2018/images/heroes/imgEmblem14.jpg', 'points' : '100'},
-        {'number' : '4', 'Teamlogo' : 'https://www.mediapia.co.kr/news/photo/202010/45766_74518_1044.png', 'points' : '180'},
-        {'number' : '5', 'Teamlogo' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points' : '80'},
-        {'number' : '6', 'Teamlogo' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points' : '10'},
-        {'number' : '7', 'Teamlogo' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points' : '150'},
-        {'number' : '8', 'Teamlogo' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points' : '100'},
-        {'number' : '9', 'Teamlogo' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points' : '100'},
-        {'number' : '10', 'Teamlogo' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points' : '100'},
+        { 'number': '1', 'Teamlogo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points': '100' },
+        { 'number': '2', 'Teamlogo': 'https://i.namu.wiki/i/nhKvnqKXbs7dbPMggKos010RqfYHxUoKgd4GOPjaN8HICce5HXZo6_zAQJPO8SNd-tMEthvTehq7ef-jzuRoww.svg', 'points': '50' },
+        { 'number': '3', 'Teamlogo': 'https://heroesbaseball.co.kr/html/front/web_2018/images/heroes/imgEmblem14.jpg', 'points': '100' },
+        { 'number': '4', 'Teamlogo': 'https://www.mediapia.co.kr/news/photo/202010/45766_74518_1044.png', 'points': '180' },
+        { 'number': '5', 'Teamlogo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points': '80' },
+        { 'number': '6', 'Teamlogo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points': '10' },
+        { 'number': '7', 'Teamlogo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points': '150' },
+        { 'number': '8', 'Teamlogo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points': '100' },
+        { 'number': '9', 'Teamlogo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points': '100' },
+        { 'number': '10', 'Teamlogo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgAQohBsdddPU6aJvkMmqVlSl5HMEs4wqpAQrtSGwCvXn4HsJcH7rrawGVpUxNckFj698&usqp=CAU', 'points': '100' },
     ];
 
     const [showAll, setShowAll] = useState(false);
@@ -67,31 +67,33 @@ export default function DonateParl() {
 
     return (
         <Container>
-            <CustomRow width='30%' alignItems='center' justifyContent='flex-start'>
+            <CustomRow width='55%' alignItems='center' justifyContent='flex-start'>
                 <CustomFont color='black' font='1.8rem' fontWeight='bold'>패럴림픽을 후원해요!</CustomFont>
             </CustomRow>
-            <CustomRow width='30%' alignItems='center' justifyContent='flex-start'>
+            <CustomRow width='55%' alignItems='center' justifyContent='flex-start'>
                 <CustomFont color='black' font='1.2rem'>가장 기부를 많이 한 야구 팬클럽은?</CustomFont>
             </CustomRow>
 
-            <CustomRow width='30%' alignItems='center' justifyContent='flex-start'>
+            <CustomRow width='55%' alignItems='center' justifyContent='flex-start'>
                 <CustomFont color='black' font='0.8rem'>30분 단위로 갱신됩니다. </CustomFont>
             </CustomRow>
             <DonateBox>
                 <CustomColumn>
                     {DanateList.slice(0, showAll ? DanateList.length : 3).map((item, index) => (
-                        <DonateFan 
-                            key={index} 
+                        <DonateFan
+                            key={index}
                             number={item.number}
-                            Teamlogo={item.Teamlogo} 
-                            points={item.points} 
+                            Teamlogo={item.Teamlogo}
+                            points={item.points}
                         />
                     ))}
                 </CustomColumn>
             </DonateBox>
-            <ToggleButton onClick={handleToggleShow}>
-                {showAll ? '간략히' : '전체보기'}
-            </ToggleButton>
+            <CustomRow width='55%' alignItems='center' justifyContent='flex-end'>
+                <ToggleButton onClick={handleToggleShow}>
+                    {showAll ? '간략히' : '전체보기'}
+                </ToggleButton>
+            </CustomRow>
         </Container>
     );
 }
--- src/pages/MainPage/MainComponents/DonateThings.jsx
+++ src/pages/MainPage/MainComponents/DonateThings.jsx
@@ -21,6 +21,12 @@ margin: 10px;
 max-width: 150px;
 text-align: center;
 cursor: pointer;
+min-height: 230px;
+min-width: 170px;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
 `;
 
 const ItemImage = styled.img`
@@ -40,48 +46,48 @@ color: #555;
 `;
 const Donatedata = [
     {
-      'img': 'https://ae01.alicdn.com/kf/S139a8230386c4dc68e22ac256c42d6bbw/TPU.jpg',
-      'text1': '마라톤 선수에게 물병 후원하기',
+        'img': 'https://ae01.alicdn.com/kf/S139a8230386c4dc68e22ac256c42d6bbw/TPU.jpg',
+        'text1': '마라톤 선수에게 물병 후원하기',
     },
     {
-      'img': 'https://sitem.ssgcdn.com/38/19/62/item/1000524621938_i1_750.jpg',
-      'text1': '수영 선수에게 팔꿈치 보호대 후원하기',
+        'img': 'https://sitem.ssgcdn.com/38/19/62/item/1000524621938_i1_750.jpg',
+        'text1': '수영 선수에게 팔꿈치 보호대 후원하기',
     },
     {
-      'img': 'https://giftinfo.co.kr/shop/item_images/zoom1/298398.jpg',
-      'text1': '높이뛰기 선수에게 쿨토시 후원하기',
+        'img': 'https://giftinfo.co.kr/shop/item_images/zoom1/298398.jpg',
+        'text1': '높이뛰기 선수에게 쿨토시 후원하기',
     },
     {
-      'img': 'https://image.auction.co.kr/itemimage/3f/83/0a/3f830a5b01.jpg',
-      'text1': '멀리뛰기 선수에게 운동화 후원하기',
+        'img': 'https://image.auction.co.kr/itemimage/3f/83/0a/3f830a5b01.jpg',
+        'text1': '멀리뛰기 선수에게 운동화 후원하기',
     },
-  ];
-  
-  const DonationItem = ({ img, text1}) => (
+];
+
+const DonationItem = ({ img, text1 }) => (
     <ItemContainer>
-      <ItemImage src={img} alt={text1} />
-      <ItemText>{text1}</ItemText>
+        <ItemImage src={img} alt={text1} />
+        <ItemText>{text1}</ItemText>
     </ItemContainer>
-  );
+);
 
 export default function DonateThings() {
-    
+
 
     return (
         <Container>
-            <CustomRow width='30%' alignItems='center' justifyContent='flex-start'>
-                <CustomFont color='black' font='1.8rem' fontWeight='bold'>이런 것들을 기부할 수 있어요.</CustomFont>
+            <CustomRow width='55%' alignItems='center' justifyContent='flex-start'>
+                <CustomFont color='black' font='1.8rem' fontWeight='bold'>이런 것들을 기부할 수 있어요!</CustomFont>
             </CustomRow>
 
-            <CustomRow>
-              {Donatedata.map((item, index) => (
-                <DonationItem 
-                  key={index} 
-                  img={item.img} 
-                  text1={item.text1} 
-                />
-              ))}
-          </CustomRow>
+            <CustomRow width='55%' alignItems='center' justifyContent='flex-start'>
+                {Donatedata.map((item, index) => (
+                    <DonationItem
+                        key={index}
+                        img={item.img}
+                        text1={item.text1}
+                    />
+                ))}
+            </CustomRow>
 
         </Container>
     );
